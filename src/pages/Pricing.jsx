a7360458b4abcf2d04446b386e95af517e5ceedf
sgc93 <smachewgedefaw@gmail.com>
2023-12-18 01:57:42 -0900

    update pricing page

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,10 +1,24 @@
-import PageLink from "../components/PageLink";
+// Uses the same styles as Product
+import styles from "./Product.module.css";
 
-export default function Pricing() {
-	return (
-		<div>
-			<PageLink />
-			<h1>AfterGo Pricing Page</h1>
-		</div>
-	);
+export default function Product() {
+  return (
+    <main className={styles.product}>
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+      </section>
+    </main>
+  );
 }

3faeeceb0fd3fe86cf963365d909a5150990e1a0
sgc93 <smachewgedefaw@gmail.com>
2023-12-18 00:48:59 -0900

    add route for page note found with path="*"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 
@@ -7,9 +8,10 @@ export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route path="/" element={<HomePage />} />
 				<Route path="product" element={<Product />} />
-				<Route path="homepage" element={<HomePage />} />
 				<Route path="pricing" element={<Pricing />} />
+				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);

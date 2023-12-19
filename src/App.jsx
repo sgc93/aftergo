5535fc7fb6a85b2b2a6b929f2d2a304750a66299
sgc93 <smachewgedefaw@gmail.com>
2023-12-18 23:47:15 -0900

    update import statement for pricing and product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/HomePage/Homepage";
 import PageNotFound from "./pages/PageNotFound";
-import Pricing from "./pages/Pricing";
-import Product from "./pages/Product";
+import Pricing from "./pages/PricingProduct/Pricing";
+import Product from "./pages/PricingProduct/Product";
 
 export default function App() {
 	return (

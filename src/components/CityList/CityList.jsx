ca2db17ba43868f7da61dbe03223793e871eb985
sgc93 <smachewgedefaw@gmail.com>
2023-12-19 02:13:37 -0900

    handle loading and empty list conditions

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,6 +1,15 @@
 import CityItem from "../CityItem/CityItem";
+import Message from "../Message/Message";
+import Spinner from "../Spinner/Spinner";
 import styles from "./CityList.module.css";
 export default function CityList({ cityList, isLoading }) {
+	if (isLoading) return <Spinner />;
+	if (!cityList.length)
+		return (
+			<Message
+				message={"Add your first city by clicking on a city on the map."}
+			/>
+		);
 	return (
 		<ul className={styles.cityList}>
 			{cityList.map((city) => (

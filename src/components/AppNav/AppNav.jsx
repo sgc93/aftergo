2184aeb8bb34463033e5d39eccffa8935e0d05ba
sgc93 <smachewgedefaw@gmail.com>
2023-12-19 01:13:45 -0900

    add navigation for visited cites list and countries list

--- src/components/AppNav/AppNav.jsx
+++ src/components/AppNav/AppNav.jsx
@@ -1,4 +1,16 @@
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 export default function AppNav() {
-	return <div className={styles.nav}>AppNavigation</div>;
+	return (
+		<div className={styles.nav}>
+			<ul>
+				<li>
+					<NavLink to="cities">cities</NavLink>
+				</li>
+				<li>
+					<NavLink to="countries">countries</NavLink>
+				</li>
+			</ul>
+		</div>
+	);
 }

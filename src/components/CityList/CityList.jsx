11dca7cec7e22f28935aec5eeae6e1fa050b1f0e
sgc93 <smachewgedefaw@gmail.com>
2023-12-19 01:56:58 -0900

    display each CityItem

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,6 +1,11 @@
-import cities from "../../data/cities";
+import CityItem from "../CityItem/CityItem";
 import styles from "./CityList.module.css";
-export default function CityList() {
-	console.log(cities);
-	return <div className={styles.cityList}>CityList</div>;
+export default function CityList({ cityList, isLoading }) {
+	return (
+		<ul className={styles.cityList}>
+			{cityList.map((city) => (
+				<CityItem city={city} key={city.id} />
+			))}
+		</ul>
+	);
 }

e533754a74d303af16fbf0faa825bae3ecc04c1a
sgc93 <smachewgedefaw@gmail.com>
2023-12-19 02:05:13 -0900

    update CityItem contents with real data

--- src/components/CityItem/CityItem.jsx
+++ src/components/CityItem/CityItem.jsx
@@ -1,4 +1,19 @@
 import styles from "./CityItem.module.css";
 export default function CityItem({ city }) {
-	return <li className={styles.cityItem}>City</li>;
+	const { cityName, emoji, date } = city;
+	const formatDate = (date) =>
+		new Intl.DateTimeFormat("en", {
+			day: "numeric",
+			month: "long",
+			year: "numeric",
+		}).format(new Date(date));
+
+	return (
+		<li className={styles.cityItem}>
+			<span className={styles.emoji}>{emoji}</span>
+			<h3 className={styles.name}>{cityName}</h3>
+			<time className={styles.date}>{formatDate(date)}</time>
+			<button className={styles.deleteBtn}>x</button>
+		</li>
+	);
 }

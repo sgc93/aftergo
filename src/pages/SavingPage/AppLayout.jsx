16dcfaf39e99684534e385a28c65d950b2fa1953
sgc93 <smachewgedefaw@gmail.com>
2023-12-19 00:49:17 -0900

    add map container component to applayout page

--- src/pages/SavingPage/AppLayout.jsx
+++ src/pages/SavingPage/AppLayout.jsx
@@ -1,3 +1,4 @@
+import Map from "../../components/Map/Map";
 import SideBar from "../../components/SideBar/SideBar";
 import styles from "./AppLayout.module.css";
 
@@ -5,6 +6,7 @@ export default function AppLayout() {
 	return (
 		<div className={styles.app}>
 			<SideBar />
+			<Map />
 		</div>
 	);
 }

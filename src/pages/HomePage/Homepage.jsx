40846a312ffa60606d8770e3ee52674dcc0b4a1b
sgc93 <smachewgedefaw@gmail.com>
2023-12-18 23:35:48 -0900

    make Homepage's content in one section element

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import PageNav from "../../components/PageNav/PageNav";
 import styles from "./Homepage.module.css";
 
 export default function Homepage() {
 	return (
 		<main className={styles.homepage}>
+			<PageNav />
 			<section>
 				<h1>
 					You travel the world.
@@ -15,10 +17,10 @@ export default function Homepage() {
 					of. Never forget your wonderful experiences, and show your friends how
 					you have wandered the world.
 				</h2>
+				<Link to="/app" className="cta">
+					Start Tracking now
+				</Link>
 			</section>
-			<Link to="/app" className="cta">
-				Start Tracking now
-			</Link>
 		</main>
 	);
 }

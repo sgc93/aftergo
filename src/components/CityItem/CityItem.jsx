e6efb8aa72d7dc7ebcf28fd280275fdba71972aa
sgc93 <smachewgedefaw@gmail.com>
2023-12-19 23:29:33 -0900

    passing query stings for each cityItems

--- src/components/CityItem/CityItem.jsx
+++ src/components/CityItem/CityItem.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 export default function CityItem({ city }) {
-	const { cityName, emoji, date, id } = city;
+	const { cityName, emoji, date, id, position } = city;
 	const formatDate = (date) =>
 		new Intl.DateTimeFormat("en", {
 			day: "numeric",
@@ -11,7 +11,10 @@ export default function CityItem({ city }) {
 
 	return (
 		<li>
-			<Link to={`${id}`} className={styles.cityItem}>
+			<Link
+				to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+				className={styles.cityItem}
+			>
 				<span className={styles.emoji}>{emoji}</span>
 				<h3 className={styles.name}>{cityName}</h3>
 				<time className={styles.date}>{formatDate(date)}</time>

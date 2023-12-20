61c4c88397b6da4b29a6f79f1efbbc77dc4de392
sgc93 <smachewgedefaw@gmail.com>
2023-12-19 23:31:53 -0900

    accessing query strings from the url to display the exact location of the city on the map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,12 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 export default function Map() {
-	return <div className={styles.mapContainer}>Map</div>;
+	const [searchParams, setSearchParams] = useSearchParams();
+	const lat = searchParams.get("lat");
+	const lng = searchParams.get("lng");
+	return (
+		<div className={styles.mapContainer}>
+			positions: {lat}, {lng}
+		</div>
+	);
 }

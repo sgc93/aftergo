3d5e8fca587696d58e9f1e77a3e7b407c86eb49e
sgc93 <smachewgedefaw@gmail.com>
2023-12-19 23:47:47 -0900

    use programatic navigation with useNavigate to display a form with a user select a city from the map by just clicking

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,14 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 export default function Map() {
-	const [searchParams, setSearchParams] = useSearchParams();
+	const [searchParams] = useSearchParams();
+	const navigateTo = useNavigate();
+
 	const lat = searchParams.get("lat");
 	const lng = searchParams.get("lng");
+
 	return (
-		<div className={styles.mapContainer}>
+		<div className={styles.mapContainer} onClick={() => navigateTo("form")}>
 			positions: {lat}, {lng}
 		</div>
 	);

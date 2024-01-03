79f4ed7146580e0cc4dd3c5ac5e9fcb3cd5f9537
sgc93 <smachewgedefaw@gmail.com>
2024-01-02 22:52:48 -0900

    add form component to add visited city detail

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,58 +2,68 @@
 
 import { useState } from "react";
 
+import { useNavigate } from "react-router-dom";
+import Button from "../Button/Button";
 import styles from "./Form.module.css";
 
 export function convertToEmoji(countryCode) {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
+	const codePoints = countryCode
+		.toUpperCase()
+		.split("")
+		.map((char) => 127397 + char.charCodeAt());
+	return String.fromCodePoint(...codePoints);
 }
 
 function Form() {
-  const [cityName, setCityName] = useState("");
-  const [country, setCountry] = useState("");
-  const [date, setDate] = useState(new Date());
-  const [notes, setNotes] = useState("");
+	const [cityName, setCityName] = useState("");
+	const [date, setDate] = useState(new Date());
+	const [notes, setNotes] = useState("");
+	const navigateTo = useNavigate();
 
-  return (
-    <form className={styles.form}>
-      <div className={styles.row}>
-        <label htmlFor="cityName">City name</label>
-        <input
-          id="cityName"
-          onChange={(e) => setCityName(e.target.value)}
-          value={cityName}
-        />
-        {/* <span className={styles.flag}>{emoji}</span> */}
-      </div>
+	return (
+		<form className={styles.form}>
+			<div className={styles.row}>
+				<label htmlFor="cityName">City name</label>
+				<input
+					id="cityName"
+					onChange={(e) => setCityName(e.target.value)}
+					value={cityName}
+				/>
+				{/* <span className={styles.flag}>{emoji}</span> */}
+			</div>
 
-      <div className={styles.row}>
-        <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
-          id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
-        />
-      </div>
+			<div className={styles.row}>
+				<label htmlFor="date">When did you go to {cityName}?</label>
+				<input
+					id="date"
+					onChange={(e) => setDate(e.target.value)}
+					value={date}
+				/>
+			</div>
 
-      <div className={styles.row}>
-        <label htmlFor="notes">Notes about your trip to {cityName}</label>
-        <textarea
-          id="notes"
-          onChange={(e) => setNotes(e.target.value)}
-          value={notes}
-        />
-      </div>
+			<div className={styles.row}>
+				<label htmlFor="notes">Notes about your trip to {cityName}</label>
+				<textarea
+					id="notes"
+					onChange={(e) => setNotes(e.target.value)}
+					value={notes}
+				/>
+			</div>
 
-      <div className={styles.buttons}>
-        <button>Add</button>
-        <button>&larr; Back</button>
-      </div>
-    </form>
-  );
+			<div className={styles.buttons}>
+				<Button type={"primary"}>Add</Button>
+				<Button
+					onClick={(e) => {
+						e.preventDefault();
+						navigateTo(-1);
+					}}
+					type={"back"}
+				>
+					&larr;
+				</Button>
+			</div>
+		</form>
+	);
 }
 
 export default Form;

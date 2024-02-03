56d225ab632891e6f949905894e31271714b031d
sgc93 <smachewgedefaw@gmail.com>
2024-02-03 00:06:25 -0900

    use context API to manage states

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import City from "./components/City/City";
 import CityList from "./components/CityList/CityList";
 import CountryList from "./components/CountyList/CountryList";
 import Form from "./components/Form/Form";
+import CitiesContextProvider from "./contexts/citiesContextProvider";
 import Homepage from "./pages/HomePage/Homepage";
 import Login from "./pages/Login/Login";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
@@ -12,21 +13,23 @@ import AppLayout from "./pages/SavingPage/AppLayout";
 
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Homepage />} />
-				<Route path="product" element={<Product />} />
-				<Route path="pricing" element={<Pricing />} />
-				<Route path="login" element={<Login />} />
-				<Route path="app" element={<AppLayout />}>
-					<Route index element={<Navigate replace to={"cities"} />} />
-					<Route path="cities" element={<CityList />} />
-					<Route path="cities/:cityId" element={<City />} />
-					<Route path="countries" element={<CountryList />} />
-					<Route path="form" element={<Form />} />
-				</Route>
-				<Route path="*" element={<PageNotFound />} />
-			</Routes>
-		</BrowserRouter>
+		<CitiesContextProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+					<Route path="product" element={<Product />} />
+					<Route path="pricing" element={<Pricing />} />
+					<Route path="login" element={<Login />} />
+					<Route path="app" element={<AppLayout />}>
+						<Route index element={<Navigate replace to={"cities"} />} />
+						<Route path="cities" element={<CityList />} />
+						<Route path="cities/:cityId" element={<City />} />
+						<Route path="countries" element={<CountryList />} />
+						<Route path="form" element={<Form />} />
+					</Route>
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</CitiesContextProvider>
 	);
 }

9899279fb97434a6d431cceb000eec1fc613ca22
sgc93 <smachewgedefaw@gmail.com>
2024-02-03 00:05:29 -0900

    move states and their logic to separe folder to manage them with context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import City from "./components/City/City";
 import CityList from "./components/CityList/CityList";
 import CountryList from "./components/CountyList/CountryList";
 import Form from "./components/Form/Form";
-import cities from "./data/cities";
 import Homepage from "./pages/HomePage/Homepage";
 import Login from "./pages/Login/Login";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
@@ -13,9 +11,6 @@ import Product from "./pages/PricingProduct/Product";
 import AppLayout from "./pages/SavingPage/AppLayout";
 
 export default function App() {
-	const [cityList] = useState(cities);
-	const [isLoading] = useState(false);
-
 	return (
 		<BrowserRouter>
 			<Routes>
@@ -25,15 +20,9 @@ export default function App() {
 				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
 					<Route index element={<Navigate replace to={"cities"} />} />
-					<Route
-						path="cities"
-						element={<CityList cityList={cityList} isLoading={isLoading} />}
-					/>
+					<Route path="cities" element={<CityList />} />
 					<Route path="cities/:cityId" element={<City />} />
-					<Route
-						path="countries"
-						element={<CountryList cityList={cityList} isLoading={isLoading} />}
-					/>
+					<Route path="countries" element={<CountryList />} />
 					<Route path="form" element={<Form />} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />

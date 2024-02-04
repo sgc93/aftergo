62c176e27a331bef5599feebab1f99a69e69a5dc
sgc93 <smachewgedefaw@gmail.com>
2024-02-03 22:18:31 -0900

    use correct name for params

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
 					<Route path="app" element={<AppLayout />}>
 						<Route index element={<Navigate replace to={"cities"} />} />
 						<Route path="cities" element={<CityList />} />
-						<Route path="cities/:cityId" element={<City />} />
+						<Route path="cities/:id" element={<City />} />
 						<Route path="countries" element={<CountryList />} />
 						<Route path="form" element={<Form />} />
 					</Route>

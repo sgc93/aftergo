9f563dc0d68543f8ada9e89769470f970763be1e
sgc93 <smachewgedefaw@gmail.com>
2024-02-04 00:05:01 -0900

    display world map using leaflet's pre-build components

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,3 +1,4 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 export default function Map() {
@@ -7,9 +8,26 @@ export default function Map() {
 	const lat = searchParams.get("lat");
 	const lng = searchParams.get("lng");
 
+	const position = [11.596431998275062, 37.400317557806964];
+
 	return (
 		<div className={styles.mapContainer} onClick={() => navigateTo("form")}>
-			positions: {lat}, {lng}
+			<MapContainer
+				center={position}
+				zoom={13}
+				scrollWheelZoom={true}
+				className={styles.map}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+				/>
+				<Marker position={position}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
 		</div>
 	);
 }

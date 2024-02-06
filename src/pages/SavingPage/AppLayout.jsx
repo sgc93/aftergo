047bb3f10072dc30e3e84302868be8ff0cd1c6e7
sgc93 <smachewgedefaw@gmail.com>
2024-02-06 00:16:46 -0900

    display logged in user in app layout page

--- src/pages/SavingPage/AppLayout.jsx
+++ src/pages/SavingPage/AppLayout.jsx
@@ -1,12 +1,16 @@
 import Map from "../../components/Map/Map";
 import SideBar from "../../components/SideBar/SideBar";
+import User from "../../components/User/User";
+import { useAuth } from "../../hooks/useAuth";
 import styles from "./AppLayout.module.css";
 
 export default function AppLayout() {
+	const { user } = useAuth();
 	return (
 		<div className={styles.app}>
 			<SideBar />
 			<Map />
+			{user && <User />}
 		</div>
 	);
 }

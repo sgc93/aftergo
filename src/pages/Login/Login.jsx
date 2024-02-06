1403cd6ecdafaf708cf1cc67e26e55a7d8ad5d31
sgc93 <smachewgedefaw@gmail.com>
2024-02-06 00:17:48 -0900

    handle login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
 import PageNav from "../../components/PageNav/PageNav";
+import { useAuth } from "../../hooks/useAuth";
 import styles from "./Login.module.css";
 
 export default function Login() {
+	const { login, isAuthenticated } = useAuth();
 	// PRE-FILL FOR DEV PURPOSES
 	const [email, setEmail] = useState("jack@example.com");
 	const [password, setPassword] = useState("qwerty");
+	const navigateTo = useNavigate();
+
+	function handleLogin(e) {
+		e.preventDefault();
+		if (email && password) login(email, password);
+	}
+
+	useEffect(() => {
+		if (isAuthenticated === true) {
+			navigateTo("/app", { replace: true });
+		}
+	}, [isAuthenticated, navigateTo]);
 
 	return (
 		<main className={styles.login}>
 			<PageNav />
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleLogin}>
 				<div className={styles.row}>
 					<label htmlFor="email">Email address</label>
 					<input
@@ -32,7 +48,7 @@ export default function Login() {
 				</div>
 
 				<div>
-					<button>Login</button>
+					<Button type={"primary"}>Login</Button>
 				</div>
 			</form>
 		</main>

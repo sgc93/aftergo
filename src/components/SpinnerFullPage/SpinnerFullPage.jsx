73d2584e1eb3da058e8b4b9400dedd74640a7255
sgc93 <smachewgedefaw@gmail.com>
2024-02-08 09:52:04 -0900

    update full page spinner for displaying while lazy loading

--- src/components/SpinnerFullPage/SpinnerFullPage.jsx
+++ src/components/SpinnerFullPage/SpinnerFullPage.jsx
@@ -1,12 +1,12 @@
-import Spinner from "./Spinner";
+import Spinner from "../Spinner/Spinner";
 import styles from "./SpinnerFullPage.module.css";
 
 function SpinnerFullPage() {
-  return (
-    <div className={styles.spinnerFullpage}>
-      <Spinner />
-    </div>
-  );
+	return (
+		<div className={styles.spinnerFullpage}>
+			<Spinner />
+		</div>
+	);
 }
 
 export default SpinnerFullPage;
